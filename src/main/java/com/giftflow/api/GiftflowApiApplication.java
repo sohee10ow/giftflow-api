96dd377aa3adf9b07e552779b6479d5dd07834a9
sohee <sh663746@kpcard.co.kr>
2026-03-31 16:12:17 +0900

    chore: add application confgi

--- src/main/java/com/giftflow/api/GiftflowApiApplication.java
+++ src/main/java/com/giftflow/api/GiftflowApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GiftflowApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GiftflowApiApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(GiftflowApiApplication.class, args);
+	}
 
 }
--- src/test/java/com/giftflow/api/GiftflowApiApplicationTests.java
+++ src/test/java/com/giftflow/api/GiftflowApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GiftflowApiApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }

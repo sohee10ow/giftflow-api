9c037706a5433b6af283579ea583ad776bddb304
sohee <sh663746@kpcard.co.kr>
2026-03-31 16:09:23 +0900

    init project create spring boot project

---
+++ src/main/java/com/giftflow/api/GiftflowApiApplication.java
@@ -0,0 +1,13 @@
+package com.giftflow.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftflowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiftflowApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/giftflow/api/GiftflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giftflow.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftflowApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

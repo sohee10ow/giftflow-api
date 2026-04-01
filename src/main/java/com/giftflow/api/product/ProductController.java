9bbe2eee37a54d5604a0bd23218fea2e05ffb1f4
Sohee10ow <sh663746@gmail.com>
2026-04-01 21:57:55 +0900

    feat: start product create endpoint

--- src/main/java/com/giftflow/api/product/ProductController.java
+++ src/main/java/com/giftflow/api/product/ProductController.java
@@ -1,13 +1,19 @@
 package com.giftflow.api.product;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
-    @GetMapping("/admin-api/v1/products")
-    public int getProduct(CreateProductRequest product) {
-        return 1;
-    }
+    private static final int TEMP_PRODUCT_ID = 1;
 
+    @PostMapping("/admin-api/v1/products")
+    public ProductResponse createProduct(@RequestBody CreateProductRequest request) {
+        return new ProductResponse(
+                TEMP_PRODUCT_ID,
+                request.getName(),
+                request.getPrice()
+        );
+    }
 }

2fdb3d3331ab005b5f77d4d60160596c92966a17
Sohee10ow <sh663746@gmail.com>
2026-04-06 22:02:56 +0900

    feat: complete basic product apis with jpa

--- src/main/java/com/giftflow/api/product/ProductController.java
+++ src/main/java/com/giftflow/api/product/ProductController.java
@@ -3,10 +3,13 @@ package com.giftflow.api.product;
 import com.giftflow.api.common.ApiError;
 import com.giftflow.api.common.ApiResponse;
 import jakarta.validation.Valid;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 @RestController
 public class ProductController {
@@ -49,5 +52,20 @@ public class ProductController {
                 product.getPrice()
         );
         return ApiResponse.success(response, UUID.randomUUID().toString());
+    }
+
+    @GetMapping("/admin-api/v1/products/all")
+    public ApiResponse<List<ProductResponse>> getProducts() {
+        List<ProductEntity> products = productRepository.findAll();
+
+       List<ProductResponse> data =  products.stream().map(
+               value -> new ProductResponse(
+                       value.getId(),
+                       value.getName(),
+                       value.getPrice()
+               )
+       ).toList();
+
+       return ApiResponse.success(data, UUID.randomUUID().toString());
     }
 }
